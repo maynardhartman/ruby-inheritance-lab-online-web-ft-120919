b734ef4bfa346ac47ef80f818ff6ae807d9c89b8
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-08 12:33:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User 
+  attr_accessor :name, :knowledge, :fname, :lname  
 
+  def initialize()
+    @name = "#{fname} #{lname}"
+    @knowledge = []
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def learn(lesson)
+    @knowledge << lesson
+  end
+  
+  def knowledge
+    return(@knowledge)
+  end
 end
---
+++ lib/user.rb
@@ -0,0 +1,8 @@
+class User 
+  attr_accessor :name 
+  attr_reader :first_name 
+  attr_writer :last_name 
+  attr_reader :last_name 
+  attr_writer :first_name
+  
+end  
